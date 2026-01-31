9e4fada5dc05463ea34f80c47d46326f7ae020e7
Administrator <Administrator@10.151.40.182>
2026-01-31 14:03:57 +0530

    test

--- wallet-service/src/main/java/com/ewallet/wallet/controller/WalletController.java
+++ wallet-service/src/main/java/com/ewallet/wallet/controller/WalletController.java
@@ -4,9 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Sample REST controller for Wallet Service
- */
 @RestController
 @RequestMapping("/api/v1/wallet")
 public class WalletController {
